c5f0f54c3ed67fbaf14bc87d1107e1153fb1bf34
tedmunds <thomase@sfu.ca>
2015-11-06 20:51:00 -0800

    Shotgun and Railgun weapons - New shotgun weapon that shoots a spread of bullets - New railgun weapon that shoots through multiple enemies

---
+++ Assets/Src/Weapons/Weapon_Railgun.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon_Railgun : Weapon {
+
+    [SerializeField]
+    public int maxPenetrations;
+
+    [SerializeField]
+    public float baseDamage;
+
+    [SerializeField] // how much does the damage get scaled by each penetration
+    public float damageFalloffPerHit;
+
+    [SerializeField]
+    public ParticleSystem shootEffectPrototype;
+
+    [SerializeField]
+    private AudioClip fireSound;
+
+    [SerializeField]
+    private AudioClip hitSound;
+
+    [SerializeField]
+    private float trailFadeOutTime;
+
+    [SerializeField]
+    private Color startTrailColor;
+
+    [SerializeField]
+    private Color endTrailColor;
+
+
+    private LineRenderer lineRenderer;
+
+	
+    protected override void Start() {
+        base.Start();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+	}
+
+
+    protected override void Update() {
+        base.Update();
+
+        float elapsed = Time.time - lastFireTime;
+        if(elapsed < trailFadeOutTime && lineRenderer != null) {
+            Color startColor = startTrailColor;
+            Color endColor = endTrailColor;
+
+            startColor.a = startTrailColor.a * (1.0f - elapsed / trailFadeOutTime);
+            endColor.a = endTrailColor.a * (1.0f - elapsed / trailFadeOutTime);
+
+            lineRenderer.SetColors(startColor, endColor);
+        }
+        else if(lineRenderer.enabled) {
+            lineRenderer.enabled = false;
+        }
+    }
+
+
+    protected override void Fire() {
+        base.Fire();
+
+        Vector3 fireOrigin = GetFireOrigin();
+        Vector3 fireDirection = owner.GetAimLocation() - fireOrigin;
+
+        Vector3 endPoint = fireOrigin + fireDirection.normalized * maxRange;
+
+        int numPenetrations = 0;
+        float currentDamage = baseDamage;
+
+        // Cast out and get all of the hits, so that it can sount through them for penetrations
+        RaycastHit2D[] hits = WeaponRayCast(fireOrigin, fireDirection, maxRange, detectLayers);
+        for(int i = 0; i < hits.Length; i++) {
+            Actor validHit = hits[i].collider.GetComponent<Actor>();
+
+            if(validHit != null) {
+                validHit.TakeDamage(currentDamage, owner, this);
+
+                numPenetrations += 1;
+                currentDamage = currentDamage * damageFalloffPerHit;
+
+                // Check if it has reached the max penetrations
+                if(numPenetrations >= maxPenetrations) {
+                    endPoint = hits[i].point;
+                    break;
+                }
+            }
+            else {
+                // Not an actor, can t penetrate it so it stops here
+                endPoint = hits[i].point;
+                Debug.Log(hits[i].collider.gameObject.name);
+                break;
+            }
+        }
+
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, fireOrigin);
+        lineRenderer.SetPosition(1, endPoint);
+
+        PlaySound(fireSound, 1.0f, Random.Range(0.85f, 1.0f));
+    }
+}
---
+++ Assets/Src/Weapons/Weapon_Shotgun.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon_Shotgun : Weapon {
+
+    [SerializeField]
+    public float spreadArc;
+
+    [SerializeField]
+    public int bulletsInSpread;
+
+    [SerializeField]
+    public ProjectileController bulletPrefab;
+
+    [SerializeField]
+    public ParticleSystem shootEffectPrototype;
+
+    [SerializeField]
+    private AudioClip fireSound;
+
+    private ParticleSystem shootEffect;
+
+    protected override void Start() {
+        base.Start();
+
+        if(shootEffectPrototype != null && shootEffect == null) {
+            shootEffect = Instantiate(shootEffectPrototype);
+            shootEffect.gameObject.SetActive(false);
+            shootEffect.transform.parent = transform;
+        }
+	}
+	
+	
+
+
+    protected override void Fire() {
+        base.Fire();
+
+        Vector3 fireDirection = GetAimDirection();
+
+        // Spawn the bullets out in a general arc
+        for(int i = 0; i < bulletsInSpread; i++) {
+            Vector3 randDirection = Random.insideUnitCircle;
+
+            Vector3 bulletDirection = Vector3.Slerp(fireDirection, randDirection, spreadArc);
+
+            GameObject spawnedBullet = WorldManager.instance.SpawnObject(bulletPrefab.gameObject, firePoint.position);
+            ProjectileController projController = spawnedBullet.GetComponent<ProjectileController>();
+            if(projController != null) {
+                projController.LaunchProjectile(bulletDirection, this);
+            }
+        }
+
+        PlaySound(fireSound, 1.0f, Random.Range(0.85f, 1.0f));
+
+        if(shootEffect != null) {
+            shootEffect.transform.position = firePoint.position;
+            shootEffect.transform.up = fireDirection;
+            shootEffect.gameObject.SetActive(true);
+            shootEffect.Play();
+        }
+    }
+}
