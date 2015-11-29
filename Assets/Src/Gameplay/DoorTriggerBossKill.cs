c3d3eded68067f75dab74c8369beb73f1cab42f2
tedmunds <thomase@sfu.ca>
2015-11-28 16:43:15 -0800

    New Menus and finished Boss Level - Added controls menu - Added new icons to upgrade menu - Added door and level exit to boss level, as well as new door trigger on boss death

---
+++ Assets/Src/Gameplay/DoorTriggerBossKill.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTriggerBossKill : MonoBehaviour {
+
+    [SerializeField]
+    private MechActor bossActor;
+
+    [SerializeField]
+    public RoomAccess_Door targetDoor;
+
+	private void Start() {
+        bossActor.RegisterDeathListener(OnBossDeath);
+	}
+
+
+
+    public void OnBossDeath(Actor died) {
+        targetDoor.Activate(FindObjectOfType<PlayerController>());
+    }
+
+}
