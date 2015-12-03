d6a1d8c04354866f75e9b79759913a474d470b9b
tedmunds <thomase@sfu.ca>
2015-12-03 14:52:31 -0800

    Better Dialogue system - Can do mulitple dialogue texts / images in a simgle trigger

--- Assets/Src/Gameplay/DialogueTrigger.cs
+++ Assets/Src/Gameplay/DialogueTrigger.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour {
+
+    [System.Serializable]
+    public class DialoguePair {
+        [SerializeField]
+        public string text;
+
+        [SerializeField]
+        public Sprite characterPortrait;
+    }
+
 
     [SerializeField]
     public string DialogueBoxTag = "UI_DialogueBox";
 
     [SerializeField]
-    public string dialogueText;
-
-    [SerializeField]
-    public Sprite characterSprite;
+    public List<DialoguePair> dialogueSequnce;
 
     private bool hasBeenTriggered;
 
@@ -20,8 +28,17 @@ public class DialogueTrigger : MonoBehaviour {
     private Text textBox;
     private Image potraitImage;
 
+    private int currentSequenceIdx;
+    private bool isActiveDialogue;
+
     private void Awake() {
         menuObject = GameObject.FindWithTag(DialogueBoxTag);
+
+        // find the next button to add the on click listener too
+        GameObject nextButtonObject = GameObject.FindWithTag("UI_DialogueNext");
+        Button nextButton = nextButtonObject.GetComponent<Button>();
+        nextButton.onClick.AddListener(() => NextDialoguePair());
+
         if(menuObject != null) {
             textBox = menuObject.GetComponentInChildren<Text>();
 
@@ -53,14 +70,49 @@ public class DialogueTrigger : MonoBehaviour {
 
             menuObject.SetActive(true);
 
-            textBox.text = dialogueText;
-            if(characterSprite != null) {
-                potraitImage.sprite = characterSprite;
+            // Set hte intial text and stuff
+            if(dialogueSequnce.Count > 0) {
+                textBox.text = dialogueSequnce[0].text;
+
+                if(dialogueSequnce[0].characterPortrait != null) {
+                    potraitImage.sprite = dialogueSequnce[0].characterPortrait;
+                    potraitImage.enabled = true;
+                }
+                else {
+                    potraitImage.enabled = false;
+                }
+            }
+            else {
+                textBox.text = "";
+                potraitImage.enabled = false;
+            }
+            isActiveDialogue = true;
+        }
+    }
+
+
+    public void NextDialoguePair() {
+        if(!hasBeenTriggered || !isActiveDialogue) {
+            return;
+        }
+
+        currentSequenceIdx += 1;
+
+        if(dialogueSequnce.Count > currentSequenceIdx) {
+            textBox.text = dialogueSequnce[currentSequenceIdx].text;
+
+            if(dialogueSequnce[currentSequenceIdx].characterPortrait != null) {
+                potraitImage.sprite = dialogueSequnce[currentSequenceIdx].characterPortrait;
                 potraitImage.enabled = true;
             }
             else {
                 potraitImage.enabled = false;
             }
+        }
+        else {
+            isActiveDialogue = false;
+            menuObject.SetActive(false);
+            WorldManager.instance.UnpauseGame();
         }
     }
 }
